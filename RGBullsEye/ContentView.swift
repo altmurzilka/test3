b5b90b2f1b8f071d6781e1d30da525fbce7732fd
altmurzilka <altmurzilka@gmail.com>
2020-05-17 02:25:44 +0600

    chapter 3

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -18,11 +18,11 @@ struct ContentView: View {
     @State var bGuess: Double
     
     func computeScore() -> Int {
-    let rDiff = rGuess - rTarget
-    let gDiff = gGuess - gTarget
-    let bDiff = bGuess - bTarget
-    let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
-    return Int((1.0 - diff) * 100.0 + 0.5)
+        let rDiff = rGuess - rTarget
+        let gDiff = gGuess - gTarget
+        let bDiff = bGuess - bTarget
+        let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+        return Int((1.0 - diff) * 100.0 + 0.5)
     }
     
     @State var showAlert = false
@@ -32,10 +32,20 @@ struct ContentView: View {
             HStack {
                 VStack {
                     Color(red: rTarget, green: gTarget, blue: bTarget)
-                    Text("Match this color")
+                    self.showAlert ? Text("R: \(Int(rTarget * 255.0))"
+                        + " G: \(Int(gTarget * 255.0))"
+                        + " B: \(Int(bTarget * 255.0))")
+                        : Text("Match this color")
                 }
                 VStack {
-                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    ZStack(alignment: .center) {
+                        Color(red: rGuess, green: gGuess, blue: bGuess)
+                        Text("60")
+                            .padding(.all, 5)
+                            .background(Color.white)
+                            .mask(Circle())
+                        .foregroundColor(.black)
+                    }
                     Text("R: \(Int(rGuess * 255.0))"
                         + " G: \(Int(gGuess * 255.0))"
                         + " B: \(Int(bGuess * 255.0))")
@@ -44,24 +54,22 @@ struct ContentView: View {
             Button(action: {self.showAlert = true}) {  // need the self because showAlert is inside a closure.
                 Text("Hit Me!")
             }.alert(isPresented: $showAlert) {
-            Alert(title: Text("Your Score"),
-            message: Text(String(computeScore())))
+                Alert(title: Text("Your Score"),
+                      message: Text(String(computeScore())))
             }.padding()
             
             VStack {
                 ColorSlider(value: $rGuess, textColor: .red)
                 ColorSlider(value: $gGuess, textColor: .green)
                 ColorSlider(value: $bGuess, textColor: .blue)
-            }
+            }.padding(.horizontal)
         }.padding(10)
-        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5).previewLayout(.fixed(width: 896, height: 414))
-        // ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
 
@@ -73,8 +81,9 @@ struct ColorSlider: View {
     var body: some View {
         HStack {
             Text("0").foregroundColor(textColor)
-            Slider(value: $value)
+            Slider(value: $value).background(textColor)
+                .cornerRadius(10)
             Text("255").foregroundColor(textColor)
-        }.padding(.horizontal)
+        }
     }
 }
